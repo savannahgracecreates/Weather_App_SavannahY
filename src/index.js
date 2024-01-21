eb883ae54dd4ca69aeec90a68142aa5191057d98
Savannah Grace <151082915+savannahgracecreates@users.noreply.github.com>
2024-01-21 18:20:52 -0500

    added date and time using javascript functions

--- src/index.js
+++ src/index.js
@@ -5,13 +5,28 @@ function refreshWeather(response) {
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windspeedElement = document.querySelector("#windspeed");
-console.log(response.data);
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
 
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
     descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windspeedElement.innerHTML = `${response.data.wind.speed} mph`;
+timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date. getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday",
+"Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    if (minutes<10) {
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes},`;
+
 }
 
 function searchCity (city) {
