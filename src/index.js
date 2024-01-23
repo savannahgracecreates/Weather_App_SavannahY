bec1cbeda0e7efb67165dafeb5e2d9ba3e8d2a31
Savannah Grace <151082915+savannahgracecreates@users.noreply.github.com>
2024-01-23 11:27:00 -0500

    Inject the Forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -41,7 +41,36 @@ function handleSearchSubmit(event) {
     let searchInput = document.querySelector("#search-form-input");
 searchCity(searchInput.value);
 }
+
+function displayForecast () {
+    let forecastElement = document.querySelector("#forecast");
+let days = ["Tue", "Wed", "Thurs", "Fri", "Sat"];
+let forecastHtml = "";
+days.forEach(function(day) {
+forecastHtml = forecastHtml +
+`
+<div class="weather-forecast-day">
+<div class="weather-forecast-date">${day}</div>
+<div class="weather-forecast-icon">☀️</div>
+<div class="weather-forecast-temperatures">
+<div class="weather-forecast-temperature">
+<strong>15°</strong>
+</div>
+<div class="weather-forecast-temperature">9°</div>
+</div>
+</div>
+`;
+});
+
+forecastElement.innerHTML = forecastHtml;
+}
+
+
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Charleston");
+displayForecast();
+
+
