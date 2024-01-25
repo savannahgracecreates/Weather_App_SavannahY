fcc71a4fb346cb05656d1d03c373a7e2ce7043b4
Savannah Grace <151082915+savannahgracecreates@users.noreply.github.com>
2024-01-25 13:55:42 -0500

    inegrated api into weather forecast using JavaScript functions and CSS

--- src/index.js
+++ src/index.js
@@ -46,6 +46,15 @@ function handleSearchSubmit(event) {
 searchCity(searchInput.value);
 }
 
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+
+
 function getForecast(city) {
   let apiKey = "d39d6443520afb97te34a49a61faf9o3";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
@@ -55,23 +64,29 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thurs", "Fri", "Sat"];
+ 
   let forecastHtml = "";
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5 ) {
     forecastHtml =
       forecastHtml +
       `
 <div class="weather-forecast-day">
-<div class="weather-forecast-date">${day}</div>
-<div class="weather-forecast-icon">☀️</div>
+<div class="weather-forecast-date">${formatDay(day.time)}</div>
+<div class="weather-forecast-icon">
+<img src= "${day.condition.icon_url}" class="weather-forecast-icon" />
+</div>
 <div class="weather-forecast-temperatures">
 <div class="weather-forecast-temperature">
-<strong>15°</strong>
+<strong>${Math.round(day.temperature.maximum)}°</strong>
 </div>
-<div class="weather-forecast-temperature">9°</div>
+<div class="weather-forecast-temperature">${Math.round(
+        day.temperature.minimum
+      )}°</div>
 </div>
 </div>
 `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
